755bfeac5e67cc74ec40a6c48a7c3486644adefb
Troy Krupinski <TroyKrupinski@my.unt.edu>
2024-04-03 17:37:55 -0500

    Changing CSS for Repos, Changing Navbar

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,6 +1,6 @@
 // NavBar.tsx
 import React from 'react';
-
+import untLogo from './UNTEng.png';
 interface NavBarProps {
     navigate: (page: string) => void; // Ensure this matches the function signature in App.tsx
 }
@@ -8,13 +8,16 @@ interface NavBarProps {
 const NavBar: React.FC<NavBarProps> = ({ navigate }) => {
     return (
         <nav>
+                     <img src={untLogo} alt="UNT College of Engineering Logo" className="unt-logo2"/>
+
+                        <h5>Troy Krupinski</h5>
+
             <button onClick={() => navigate('resume')}>Resume</button>
             <button onClick={() => navigate('researchPapers')}>Research Papers</button>
 
             <button onClick={() => navigate('home')}>Home</button>
             <button onClick={() => navigate('contact')}>Contact</button>
             <button onClick={() => navigate('repositories')}>Repositories</button>
-
         </nav>
     );
 };
