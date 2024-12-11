b79bda2ced9a1e0d93e6f1a31a0d561a2d6d81ef
Troy Krupinski <43382791+TroyKrupinski@users.noreply.github.com>
2024-12-10 19:17:45 -0600

    Update ResearchPapers.tsx

--- src/ResearchPapers.tsx
+++ src/ResearchPapers.tsx
@@ -4,6 +4,11 @@ import ResearchPaper from './ResearchPaper';
 const ResearchPapers: React.FC = () => {
     const papers = [
         {
+            title: 'Objective Classification of Hemiplegic Cerebral Palsy: Objective Cluster Analysis of Whole Time Series Gait Data',
+            summary: 'Using machine learning to classify hemiplegia in cerebral palsy patients based on gait data.',
+            pdfLink: 'https://digital.library.unt.edu/ark:/67531/metadc2375434/',
+        },
+        {
             title: 'Applied Artificial Intelligence in Charity Organization Donations',
             summary: 'Using AI to predict donation amounts for charity organizations.',
             pdfLink: '/chairty.pdf',
@@ -17,7 +22,6 @@ const ResearchPapers: React.FC = () => {
             title: 'Predictive Analytics in Hospital Data with Machine Learning',
             summary: 'Using machine learning to predict patient length of stay based on race and total cost using real hospital data.',
             pdfLink: '/quantum-computing.pdf',
-            
         },
         // ...other papers
     ];
